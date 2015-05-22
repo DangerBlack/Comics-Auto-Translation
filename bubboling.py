ff7ab6cdd6ae6b6d9718d8283652b8bcbb01e206
DangerBlack <danger.recheng@hotmail.it>
2015-05-22 17:36:06 +0200

    added the licence, today I can't do it!

--- bubboling.py
+++ bubboling.py
@@ -4,12 +4,12 @@
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    DeviantArtWatcher is distributed in the hope that it will be useful,
+    Bubboling is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with  WhatsApp Dragon.  If not, see <http://www.gnu.org/licenses/>.
+    along with Bubboling.  If not, see <http://www.gnu.org/licenses/>.
     
     @author DangerBlack
     @version 0.1
